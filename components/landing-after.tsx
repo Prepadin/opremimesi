94bf006ab5258e8e7a902ef9ec3ce265e093f46b
unknown <prepaddoo@gmail.com>
2024-11-15 17:48:11 +0100

    third commit

--- components/landing-after.tsx
+++ components/landing-after.tsx
@@ -1,7 +1,113 @@
+// import Image from 'next/image'
+// import { Button } from "@/components/ui/button"
+
+// export const LandingAfter = () => {
+//   return (
+//     <div className="container mx-auto px-4 py-8 md:py-16">
+//       <div className="flex flex-col md:flex-row items-center justify-between">
+//         <div className="md:w-1/2 mb-8 md:mb-0">
+//           {/* <div className="bg-gray-100 text-gray-600 text-sm py-2 px-4 rounded-full inline-block mb-4">
+//             Used by over <span className="text-blue-500 font-semibold">2 million people</span> to redesign homes
+//           </div> */}
+//           <h1 className="text-4xl md:text-5xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+//           Vaš osebni <span className="text-blue-500">AI</span><br />
+//           notranji oblikovalec
+//           </h1>
+//           {/* <Button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-full">
+//             Redesign your room
+//             <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+//               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+//             </svg>
+//           </Button> */}
+//         </div>
+//         <div className="md:w-1/2 relative">
+//           <div className="bg-white rounded-lg shadow-lg p-4">
+//             {/* <div className="absolute top-4 right-8 bg-white text-blue-500 font-semibold py-1 px-3 rounded-full shadow z-10">
+//               Instantly Redesign
+//             </div> */}
+//             <div className="flex">
+//               <div className="w-1/2 pr-2">
+//                 <Image
+//                   src="/bed.jpg?height=300&width=200"
+//                   alt="Before room design"
+//                   width={300}
+//                   height={400}
+//                   className="rounded-lg"
+//                 />
+//                 <div className="mt-2 text-center text-gray-600 font-semibold">Prej</div>
+//               </div>
+//               <div className="w-1/2 pl-2">
+//                 <Image
+//                   src="/lux1.png?height=300&width=200"
+//                   alt="After room design"
+//                   width={300}
+//                   height={400}
+//                   className="rounded-lg"
+//                 />
+//                 <div className="mt-2 text-center text-gray-600 font-semibold">Potem</div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+"use client"
+
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
+import { motion } from "framer-motion";
+import { useState, useEffect } from 'react'
+
 
 export const LandingAfter = () => {
+  const [isFlipped, setIsFlipped] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const images = [
+    { src: "/room5.jpg?height=300&width=200", alt: "Image 5", text: "Image 5" },
+    {  src: "/living2.jpg?height=300&width=200", alt: "Image 4", text: "Image 4" },
+    { src: "/dinning.jpg?height=300&width=200", alt: "Image 3", text: "Image 3" },
+    { src: "/room4.jpg?height=300&width=200", alt: "Image 4", text: "Image 4" },
+    { src: "/kitchen.jpg?height=300&width=200", alt: "Image 5", text: "Image 5" },
+  ]
+
+  const images1 = [
+    { src: "/room5lux.png?height=300&width=200", alt: "Image 5", text: "Image 5" },
+    {  src: "/livinglux3.png?height=300&width=200", alt: "Image 4", text: "Image 4" },
+    { src: "/livinglux.png?height=300&width=200", alt: "Image 3", text: "Image 3" },
+    { src: "/room4lux.png?height=300&width=200", alt: "Image 4", text: "Image 4" },
+    { src: "/kitchenlux.png?height=300&width=200", alt: "Image 5", text: "Image 5" },
+  ]
+
+
+
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setIsFlipped(prev => !prev)
+  //   }, 5000) // Flip every 5 seconds
+
+  //   return () => clearInterval(intervalId) // Cleanup on unmount
+  // }, [])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }, 5000) // Flip every 5 seconds
+
+    return () => clearInterval(intervalId) // Cleanup on unmount
+  }, [])
+
+  const handleManualFlip = (direction: 'next' | 'prev') => {
+    if (direction === 'next') {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    } else {
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
+    }
+  }
+
   return (
     <div className="container mx-auto px-4 py-8 md:py-16">
       <div className="flex flex-col md:flex-row items-center justify-between">
@@ -27,23 +133,37 @@ export const LandingAfter = () => {
             </div> */}
             <div className="flex">
               <div className="w-1/2 pr-2">
+              <motion.div
+           key={currentIndex}
+           initial={{ opacity: 0, rotateY: -180 }}
+           animate={{ opacity: 1, rotateY: 0 }}
+           exit={{ opacity: 0, rotateY: 180 }}
+           transition={{ duration: 0.5 }}>
                 <Image
-                  src="/bed.jpg?height=300&width=200"
-                  alt="Before room design"
+                 src={images[currentIndex].src}
+                 alt={images[currentIndex].alt}
                   width={300}
-                  height={400}
+                  height={200}
                   className="rounded-lg"
                 />
-                <div className="mt-2 text-center text-gray-600 font-semibold">Prej</div>
+                </motion.div>
+                <div className="mt-2 text-center text-gray-600 font-semibold">Pred</div>
               </div>
               <div className="w-1/2 pl-2">
+              <motion.div
+           key={currentIndex}
+           initial={{ opacity: 0, rotateY: -180 }}
+           animate={{ opacity: 1, rotateY: 0 }}
+           exit={{ opacity: 0, rotateY: 180 }}
+           transition={{ duration: 0.5 }}>
                 <Image
-                  src="/lux1.png?height=300&width=200"
-                  alt="After room design"
+                 src={images1[currentIndex].src}
+                 alt={images1[currentIndex].alt}
                   width={300}
-                  height={400}
+                  height={200}
                   className="rounded-lg"
                 />
+                </motion.div>
                 <div className="mt-2 text-center text-gray-600 font-semibold">Potem</div>
               </div>
             </div>

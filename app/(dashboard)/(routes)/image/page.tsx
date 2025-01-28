ece4bad084a1007571ceba18fc3f36895070d323
unknown <prepaddoo@gmail.com>
2025-01-28 15:23:58 +0100

    third commit

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -146,7 +146,7 @@ export default function ImagePage() {
       // Step 3: Generate the image with the translated prompt
       setLoading(true);
       const imageResponse = await axios.post(
-        `https://c3eb-46-122-68-255.ngrok-free.app/generate_design/?prompt=${translatedPrompt}`,
+        `https://dc4f-46-122-68-255.ngrok-free.app/generate_design/?prompt=${translatedPrompt}`,
         formData,
         {
           headers: {

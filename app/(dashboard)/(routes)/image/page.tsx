e5f9741ed92919f450d8e5494a2fc2b4cffec99a
unknown <prepaddoo@gmail.com>
2025-03-10 08:05:46 +0100

    First commit

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -260,7 +260,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     formDataForDesign.append("image", compressedFile); // Use the compressed file for AI generation
 
     const imageResponse = await axios.post(
-      `https://0965-46-122-71-78.ngrok-free.app/generate_design/?prompt=${translatedPrompt}`,
+      `https://7128-46-122-71-78.ngrok-free.app/generate_design/?prompt=${translatedPrompt}`,
       formDataForDesign,
       {
         headers: {
